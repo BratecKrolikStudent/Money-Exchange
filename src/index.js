9946cc033282a4f08f231bcabff82388b4295bc7
Dmitriy Kunets <t38@tut.by>
2018-09-11 12:11:58 +0300

    task completed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,34 @@
 // PLEASE DON'T change function name
+function getDetermine(currency, denomination) {
+  return { 
+    remainder: currency % denomination,
+    count: currency / denomination | 0
+  }
+}
+
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+  const COINS = [
+    { letter: 'H', denomination: 50 },
+    { letter: 'Q', denomination: 25 },
+    { letter: 'D', denomination: 10 },
+    { letter: 'N', denomination: 5 },
+    { letter: 'P', denomination: 1 },
+  ]
+  let result = {};
+  
+  if (currency > 10000) {
+    result = { error: "You are rich, my friend! We don't have so much coins for exchange" }
+  } else {
+    let money = currency;
+
+    for (let coin of COINS) {
+      let { remainder, count } = getDetermine(money, coin.denomination);
+
+      money = remainder;
+      if (count > 0) {
+        result[coin.letter] = count
+      }
+    }
+  }
+  return result;
 }
